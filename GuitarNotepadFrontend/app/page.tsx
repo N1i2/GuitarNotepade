693e978d32b5b2f9d463d141f8494e83388287d4
Nikolay Selicky <109921087+N1i2@users.noreply.github.com>
2025-10-31 13:56:42 +0300

    created next project

---
+++ GuitarNotepadFrontend/app/page.tsx
@@ -0,0 +1,9 @@
+import { ModeToggle } from "@/components/toggle";
+
+export default function Home() {
+  return (
+    <div className="flex bg-destructive dark:bg-chart-2">
+        <ModeToggle></ModeToggle>
+    </div>
+  );
+}
